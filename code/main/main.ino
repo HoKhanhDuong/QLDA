985980cf3d359a5a449726a888d684b2ceec900f
duonghk-0127 <ho.khanh.duong@sun-asterisk.com>
2021-06-02 10:38:48 +0700

    done code

--- code/main/main.ino
+++ code/main/main.ino
@@ -1,21 +1,17 @@
 
-byte offset = 10;
-
-byte SpMaxL=86 + offset;
-byte SpMaxR=76 + offset;
+byte SpMaxL=92;
+byte SpMaxR=86;
 #define inA1 6
 #define inA2 7
 #define inB1 8
 #define inB2 9
 
-int stat = 200;
-int vach = 0;
+int trang_thai_cu = 200;
 
 void Tien( byte speedL, byte speedR);
-void Lui(byte SpeedL, byte SpeedR);
-void doline();
+void doLine();
 int trangThai5CamBien ();
-int lechvach5mat(int sens1, int sens2, int sens3, int sens4, int sens5);
+int lechVach(int sens1, int sens2, int sens3, int sens4, int sens5);
 
 void setup() {
   Serial.begin(9600);
@@ -34,26 +30,20 @@ void setup() {
   digitalWrite(inA2, LOW);
   digitalWrite(inB1, HIGH);
   digitalWrite(inB2, LOW); 
-  
-  Tien(100,85);
-  delay(5);
 }
 
 void loop() {
-  doline();
-
+  doLine();
 }
 
-void doline()
+void doLine()
 {
-   int trangthai = trangThai5CamBien();
-//  Serial.println(trangthai);
+   int trang_thai = trangThai5CamBien();
 
- if (trangthai == 0 && stat != 200) { // thử bỏ cái này 
-    trangthai = stat;
+ if (trang_thai == 0 && trang_thai_cu != 200) {
+    trang_thai = trang_thai_cu;
  }
-// trangthai=200;
-  switch (trangthai)
+  switch (trang_thai)
   {
     case 200:
       Tien(SpMaxL,SpMaxR);
@@ -86,9 +76,7 @@ void doline()
       digitalWrite(9, 0);
       break;
   }
-  Serial.println(trangthai);
-  stat = trangthai;
-//  delay(100);
+  trang_thai_cu = trang_thai;
 }
 boolean IFSensor (byte PinNumb)
 {
@@ -103,64 +91,22 @@ int trangThai5CamBien ()
       Dương là lệch trái
       Mức độ lệch giảm tăng dần : 1 2 3
   */
-  int sens1 = IFSensor(A4);
+  int sens1 = IFSensor(A4); // góc trái 
   int sens2 = IFSensor(A3);
   int sens3 = IFSensor(A2);
   int sens4 = IFSensor(A1);
   int sens5 = IFSensor(A0);
-//   if (sens1 == 0 && sens5 == 0){
-//    vach = 1;
-//  }
-//  if (sens1 == 1 && sens5 == 1){
-//    vach = 0;
-//  }
-//  if (vach == 0){
-//    sens1 = !sens1;
-//    sens2 = !sens2;
-//    sens3 = !sens3;
-//    sens4 = !sens4;
-//    sens5 = !sens5;
-//  }
-//  Serial.print(sens1);
-//  Serial.print(" ");
-//  Serial.print(sens2);
-//  Serial.print(" ");
-//  Serial.print(sens3);
-//  Serial.print(" ");
-//  Serial.print(sens4);
-//  Serial.print(" ");
-//  Serial.println(sens5); 
-//  delay(500);
-  switch (lechvach5mat(sens1, sens2, sens3, sens4, sens5))
-  {
-    case -3:
-      return -3;
-      break;
-    case -2:
-      return -2;
-      break;
-    case -1:
-      return -1;
-      break;
-    case 0:
-      if ( (abs(sens1) + abs(sens2) + abs(sens3) + abs(sens4) + abs(sens5)) == 0) // Trạng thái lệch hẳn khỏi line
-        return 0;
-      else 
-        return 200;
-      break;
-    case 1:
-      return 1;
-      break;
-    case 2:
-      return 2;
-      break;
-    case 3:
-      return 3;
-      break;
 
+  int do_lech = lechVach(sens1, sens2, sens3, sens4, sens5);
+
+  if (do_lech == 0){
+    if ((sens1 + sens2 + sens3 + sens4 + sens5) != 0) return 200;
   }
+
+  return do_lech;
+
 }
-int lechvach5mat(int sens1, int sens2, int sens3, int sens4, int sens5)
+int lechVach(int sens1, int sens2, int sens3, int sens4, int sens5)
 {
   /*
       Âm là lệch phải
@@ -168,8 +114,8 @@ int lechvach5mat(int sens1, int sens2, int sens3, int sens4, int sens5)
       Mức độ lệch giảm tăng dần : 1 3 2
   */
 
-  int lechvach = -2 * sens1 - sens2 + sens4 + 2 * sens5;
-  switch (lechvach)
+  int do_lech = -2 * sens1 - sens2 + sens4 + 2 * sens5;
+  switch (do_lech)
   {
     case -3:// cái này là do 2 mắt biên đều chạm vạch
       return -2;
@@ -192,7 +138,6 @@ int lechvach5mat(int sens1, int sens2, int sens3, int sens4, int sens5)
     case 3:
       return 2;
       break;
-
   }
 
 }
@@ -205,30 +150,3 @@ void Tien( byte speedL, byte speedR)
   digitalWrite(inB1, HIGH);
   digitalWrite(inB2, LOW);
 }
-//void Retrai()
-//{
-//  analogWrite(5, 255);
-//  analogWrite(10, 255);
-//  digitalWrite(6, 0);
-//  digitalWrite(7, 0);
-//  digitalWrite(8, 1);
-//  digitalWrite(9, 0);
-//}
-//void Rephai()
-//{
-//  analogWrite(5, 255);
-//  analogWrite(10, 255);
-//  digitalWrite(6, 1);
-//  digitalWrite(7, 0);
-//  digitalWrite(8, 0);
-//  digitalWrite(9, 0);
-//}
-void Lui(byte SpeedL, byte SpeedR)
-{
-  analogWrite(5, SpeedL);
-  analogWrite(10, SpeedR);
-  digitalWrite(6, 0);
-  digitalWrite(7, 1);
-  digitalWrite(8, 0);
-  digitalWrite(9, 1);
-} 
